b27c449a61ad551c03f716f538933f016fa9eb6a
Ubuntu <Terry.Ott@publicntp.org>
2019-02-18 22:40:14 +0000

    Starting API unit test

---
+++ api/test/server_add/serverAdd.py
@@ -0,0 +1,46 @@
+import boto3
+
+def _clearTestData():
+    pass
+
+
+def _validateServerAddedCorrectly(serverData):
+    pass
+
+
+def _getNewServerData():
+
+    serversToAdd = [
+        {
+            'server_address'        : 'server1.unittest',
+            'display_name'          : 'DisplayName for server1.unittest',
+            'display_description'   : 'Description for server1.unittest',
+            'display_location'      : 'Locaiton for server1.unittest',
+            'notes'                 : 'Notes for server1.unittest'
+        },
+    ]
+
+    return serversToAdd
+
+
+def _addServer(serverDetails):
+    pass
+
+
+def _addNewServers(serversToAdd):
+    for currServer in serversToAdd:
+        _addServer(currServer)
+        _validateServerAddedCorrectly(currServer)
+
+   
+
+def main():
+    newServers = _getNewServerData()
+
+    _clearTestData()
+
+    _addNewServers(newServers)
+
+
+if __name__ == "__main__":
+    main()
